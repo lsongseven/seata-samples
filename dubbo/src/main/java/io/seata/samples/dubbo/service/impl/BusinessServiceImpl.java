b8402ddacd5e94c7aee62313e87544be78e7b7be
lsongseven <lsongseven@gmail.com>
2020-12-08 19:02:00 +0800

    local config

--- dubbo/src/main/java/io/seata/samples/dubbo/service/impl/BusinessServiceImpl.java
+++ dubbo/src/main/java/io/seata/samples/dubbo/service/impl/BusinessServiceImpl.java
@@ -42,8 +42,8 @@ public class BusinessServiceImpl implements BusinessService {
     public void purchase(String userId, String commodityCode, int orderCount) {
         LOGGER.info("purchase begin ... xid: " + RootContext.getXID());
         storageService.deduct(commodityCode, orderCount);
-        orderService.create(userId, commodityCode, orderCount);
-        //throw new RuntimeException("xxx");
+        throw new RuntimeException("xxx");
+//        orderService.create(userId, commodityCode, orderCount);
 
     }
 
